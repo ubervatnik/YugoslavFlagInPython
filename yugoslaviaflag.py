7581b474cd37c6d6e6fa0bf8357fa778d49570f8
trumpisbased2024 <deloox421@gmail.com>
2024-08-16 20:37:43 +0300

    Update yugoslaviaflag.py

--- yugoslaviaflag.py
+++ yugoslaviaflag.py
@@ -1,6 +1,6 @@
 def yugoslav_flag():
-    colors = ["\033[48;5;196m", "\033[48;5;27m", "\033[48;5;255m"]  # Red, blue, and white background colors
-    for i in range(1, 4):  # Keep the number of rows as before
+    colors = ["\033[48;5;196m", "\033[48;5;27m", "\033[48;5;255m"]
+    for i in range(1, 4):  
         print(colors[i % 3] + " " * 12 + "\033[0m")
 
 yugoslav_flag()
